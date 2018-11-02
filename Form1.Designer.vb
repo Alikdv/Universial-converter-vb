147af6045e0f591d3f5f2fbd1a3c7e68425e6f62
Arik <alikdv@gmail.com>
2018-11-01 23:58:56 -0400

    new upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.datataConvertIn = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.resultOut = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(171, 76)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(208, 25)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 76)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Select type of Unit"
+        '
+        'datataConvertIn
+        '
+        Me.datataConvertIn.Location = New System.Drawing.Point(171, 177)
+        Me.datataConvertIn.Name = "datataConvertIn"
+        Me.datataConvertIn.Size = New System.Drawing.Size(91, 23)
+        Me.datataConvertIn.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 177)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Data to convert"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(279, 177)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Convert   >> "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(34, 228)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Result"
+        '
+        'resultOut
+        '
+        Me.resultOut.Location = New System.Drawing.Point(171, 228)
+        Me.resultOut.Name = "resultOut"
+        Me.resultOut.Size = New System.Drawing.Size(91, 23)
+        Me.resultOut.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(29, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(389, 46)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Universal Converter"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(33, 270)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(385, 110)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Description"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 129)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Convert to"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(171, 129)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(208, 26)
+        Me.RichTextBox1.TabIndex = 12
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.resultOut)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.datataConvertIn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Universal Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents datataConvertIn As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents resultOut As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,98 @@
+﻿'Project: The Universal Converter
+'programm to do unit conversions.
+'Copyright(C) 2018 Alexei Dvorezki
+'email: alikdv@gmail.com
+'This program Is free software: you can redistribute it And/Or modify
+'it under the terms Of the GNU General Public License As published by
+'the Free Software Foundation, either version 3 Of the License, Or
+'any later version.
+
+'This program Is distributed In the hope that it will be useful,
+'but WITHOUT ANY WARRANTY; without even the implied warranty Of
+'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'GNU General Public License For more details.
+
+'See <https://www.gnu.org/licenses/> to receive a copy of the GNU General Public Licen
+
+Public Class Form1
+
+    Dim Celsius As String = "*************************** Formula: °C x 1.8 + 32 = °F *****"
+    Dim Centimeter As String = "************************ Formula: cm * 2.54 = Inches *****"
+    Dim Hours As String = "*************************** Formula: Hours * 60 = Minutes *****"
+    Dim Kilometer As String = "*********************** Formula: km * 1000 = Meter *****"
+
+    Dim Number As Integer
+    Dim Answer As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Celsius")
+        ComboBox1.Items.Add("Centimeter")
+        ComboBox1.Items.Add("Hours")
+        ComboBox1.Items.Add("Kilometer")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.SelectedItem = "Celsius" Then
+            Number = datataConvertIn.Text
+            Answer = Number * 1.8 + 32
+            resultOut.Text = FormatNumber(Answer)
+
+        End If
+        If ComboBox1.SelectedItem = "Centimeter" Then
+            Number = datataConvertIn.Text
+            Answer = Number * 2.54
+            resultOut.Text = FormatNumber(Answer)
+        End If
+        If ComboBox1.SelectedItem = "Hours" Then
+            Number = datataConvertIn.Text
+            Answer = Number * 60
+            resultOut.Text = FormatNumber(Answer)
+        End If
+        If ComboBox1.SelectedItem = "Kilometer" Then
+            Number = datataConvertIn.Text
+            Answer = Number * 1000
+            resultOut.Text = FormatNumber(Answer)
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        If ComboBox1.SelectedItem = "Celsius" Then
+            RichTextBox1.Text = ("Fahrenheir")
+            GroupBox1.Text = ComboBox1.SelectedItem & "(" & Celsius & ")"
+
+        End If
+        If ComboBox1.SelectedItem = "Centimeter" Then
+            RichTextBox1.Text = ("Inches")
+            GroupBox1.Text = ComboBox1.SelectedItem & "(" & Centimeter & ")"
+        End If
+        If ComboBox1.SelectedItem = "Hours" Then
+            RichTextBox1.Text = ("Minutes")
+            GroupBox1.Text = ComboBox1.SelectedItem & "(" & Hours & ")"
+        End If
+        If ComboBox1.SelectedItem = "Kilometer" Then
+            RichTextBox1.Text = ("Metre")
+            GroupBox1.Text = ComboBox1.SelectedItem & "(" & Kilometer & ")"
+        End If
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub datataConvertIn_TextChanged(sender As Object, e As EventArgs) Handles datataConvertIn.TextChanged
+
+    End Sub
+End Class
